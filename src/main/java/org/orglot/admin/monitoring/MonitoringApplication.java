60d256e8c463244d9b8db5419e685505884b855e
yk <yk@finch-melrose.com>
2020-03-09 19:34:26 +0300

    add @EnableAdminServer

--- src/main/java/org/orglot/admin/monitoring/MonitoringApplication.java
+++ src/main/java/org/orglot/admin/monitoring/MonitoringApplication.java
@@ -1,8 +1,10 @@
 package org.orglot.admin.monitoring;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class MonitoringApplication {
 
